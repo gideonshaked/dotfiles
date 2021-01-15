b3b13e6d9674bbc15aff926939c6c32a95be3baa
The-Kid-Gid <gideonshaked@gmail.com>
2021-01-14 19:23:51 -0500

    Remove condawrapper

--- system/external.bash
+++ system/external.bash
@@ -4,5 +4,3 @@ export WORKON_HOME="$HOME/.virtualenvs"
 export PROJECT_HOME="$HOME/git"
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 source "$HOME/.local/bin/virtualenvwrapper.sh"
-
-source "$HOME/.local/bin/condawrapper.sh"

2be113a5b53854d6c9f3d45ac012e2ebf308103a
The-Kid-Gid <gideonshaked@gmail.com>
2021-01-21 21:19:00 -0500

    Fix path order

--- system/path.bash
+++ system/path.bash
@@ -4,8 +4,8 @@
 
 path_prepend "$HOME/.local/bin:$HOME/bin"
 
+path_append "$HOME/.cargo/bin"
+
 path_append "/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin"
 
 path_append "$PROJECTS/wscli/src"
-
-path_append "$HOME/.cargo/bin"

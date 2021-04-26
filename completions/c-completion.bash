656bd8c2474f8cc0b2b6a88cfbc8b82978f86e0f
The-Kid-Gid <gideonshaked@gmail.com>
2021-04-25 20:46:01 -0400

    Spacing

--- completions/c-completion.bash
+++ completions/c-completion.bash
@@ -8,4 +8,4 @@ _c_completions() {
     COMPREPLY=($(compgen -W "$(find $PROJECTS -mindepth 1  -maxdepth 1 -type d | awk '{n=split($NF,a,"/");print $5,a[n]}')" -- "${COMP_WORDS[1]}"))
 }
 
-complete -F _c_completions  c
+complete -F _c_completions c

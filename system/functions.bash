e2c577c618fc5ff3099981adb502be8092385d5e
The-Kid-Gid <gideonshaked@gmail.com>
2021-04-25 21:36:18 -0400

    Remove 'here'

--- system/functions.bash
+++ system/functions.bash
@@ -54,18 +54,6 @@ dfu() {
     cd "$PROJECTS/dotfiles" && git pull --ff-only && ./install-profile "$(hostname)"
 }
 
-# Print current directory following symlinks
-here() {
-    local loc
-    if [ "$#" -eq 1 ]; then
-        loc=$(realpath "$1")
-    else
-        loc=$(realpath ".")
-    fi
-    ln -sfn "${loc}" "$HOME/.shell.here"
-    echo "here -> $(readlink $HOME/.shell.here)"
-}
-
 # Remove from PATH
 path_remove() {
     PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')

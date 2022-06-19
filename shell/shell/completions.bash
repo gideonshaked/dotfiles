a51050054480adad04212ad21762f4f61bd24cea
Gideon Shaked <gideonshaked@gmail.com>
2022-06-19 18:27:34 -0400

    Clean up completions file

--- shell/shell/completions.bash
+++ shell/shell/completions.bash
@@ -2,13 +2,13 @@
 # Custom completion functions
 #
 
-# For shell function latexwc (file: functions.bash)
-# Possible args are .tex files under the current directory
-_latexwc_completion() {
-    COMPREPLY=$(compgen -W "$(find . -type f)")
-}
-
-complete -F _latexwc_completion latexwc
-
 # Autocomplete for aws-cli (different from auto-prompt)
 complete -C "/usr/bin/aws_completer" aws
+
+# Autocomplete for v and c functions (file: functions.bash)
+_v_and_c_completion() {
+    COMPREPLY=($(compgen -W "$(find "$PROJECTS/" -type d | cut -d "/" -f5-)" "${COMP_WORDS[1]}"))
+}
+
+complete -F _v_and_c_completion v
+complete -F _v_and_c_completion c

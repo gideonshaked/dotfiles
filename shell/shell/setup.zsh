8db0d1c3355fd029e2b2936f23b5d73d65cec94f
Gideon Shaked <gideonshaked@gmail.com>
2022-08-09 18:17:38 -0400

    Add wakatime

--- shell/shell/setup.zsh
+++ shell/shell/setup.zsh
@@ -73,3 +73,6 @@ export LESS_TERMCAP_so=$'\E[01;47;34m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[01;36m'
 export LESS=-R
+
+# Wakatime
+export ZSH_WAKATIME_PROJECT_DETECTION=true

4bd94766a434100b847c7f068b682e69cc41ed34
Gideon Shaked <gideonshaked@gmail.com>
2022-08-11 15:44:44 -0400

    Formatting

--- shell/shell/zsh_config.zsh
+++ shell/shell/zsh_config.zsh
@@ -1,25 +1,26 @@
 #
-# Configure Zsh options and plugins
+# Configure Zsh
 #
 
-ZSH=/usr/share/oh-my-zsh # Oh-my-zsh
+ZSH=/usr/share/oh-my-zsh  # Oh-my-zsh
 
-## Prompt
+## Powerlevel10k
 source "$HOME/.p10k.zsh"
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 
 ## Options
-setopt correct                  # Auto correct mistakes
-setopt extendedglob             # Extended globbing. Allows using regular expressions with *
-setopt nocaseglob               # Case insensitive globbing
-setopt rcexpandparam            # Array expension with parameters
-setopt nocheckjobs              # Don't warn about running processes when exiting
-setopt numericglobsort          # Sort filenames numerically when it makes sense
-setopt nobeep                   # No beep
-setopt inc_append_history       # Immediately append history instead of overwriting
-setopt histignorealldups        # If a new command is a duplicate, remove the older one
-setopt autocd                   # if only directory path is entered, cd there.
-setopt inc_append_history       # save commands are added to the history immediately, otherwise only when shell exits.
-setopt histignorespace          # Don't save commands that start with space
+setopt correct                  								# Auto correct mistakes
+setopt extendedglob             								# Extended globbing. Allows using regular expressions with *
+setopt nocaseglob               								# Case insensitive globbing
+setopt rcexpandparam            								# Array expension with parameters
+setopt nocheckjobs              								# Don't warn about running processes when exiting
+setopt numericglobsort          								# Sort filenames numerically when it makes sense
+setopt nobeep                   								# No beep
+setopt inc_append_history       								# Immediately append history instead of overwriting
+setopt histignorealldups        								# If a new command is a duplicate, remove the older one
+setopt autocd                   								# if only directory path is entered, cd there.
+setopt inc_append_history       								# save commands are added to the history immediately, otherwise only when shell exits.
+setopt histignorespace          								# Don't save commands that start with space
 
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'       # Case insensitive tab completion
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"         # Colored completion (different colors for dirs/files/etc)
@@ -44,6 +45,3 @@ plugins=(
 )
 source $ZSH/oh-my-zsh.sh  # Oh-my-zsh
 source $ZSH/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh  # Syntax highlighting
-
-## Theme
-source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme

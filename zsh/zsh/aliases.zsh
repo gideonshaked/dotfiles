7cb5dc4305280ee5d90c0a9d69b9d501122f34d2
Gideon Shaked <gideonshaked@gmail.com>
2022-08-14 00:59:29 -0400

    Remove unused alias

--- zsh/zsh/aliases.zsh
+++ zsh/zsh/aliases.zsh
@@ -12,9 +12,6 @@ alias batt="upower -i $(upower -e | grep BAT) | grep --color=never -E 'state|to\
 alias python="python3"
 alias pip="pip3"
 
-# Get lines of source code for project
-alias sloc="git ls-files \"*.py\" \"*.java\" \"*.sh\" \"*.bash\" \"*.ps1\" \"*.js\" | xargs wc -l"
-
 # Pipe output of command to clipboard
 alias clip="xclip -selection c"
 

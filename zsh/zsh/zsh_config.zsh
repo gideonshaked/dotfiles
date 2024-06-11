4d27244323bcbbd62720ea94dcc0e53ea49f6abf
Gideon Shaked <gideonshaked@gmail.com>
2024-06-11 19:25:44 -0400

    fix spelling

--- zsh/zsh/zsh_config.zsh
+++ zsh/zsh/zsh_config.zsh
@@ -6,7 +6,7 @@
 setopt correct                  								# Auto correct mistakes
 setopt extendedglob             								# Extended globbing. Allows using regular expressions with *
 setopt nocaseglob               								# Case insensitive globbing
-setopt rcexpandparam            								# Array expension with parameters
+setopt rcexpandparam            								# Array expansion with parameters
 setopt nocheckjobs              								# Don't warn about running processes when exiting
 setopt numericglobsort          								# Sort filenames numerically when it makes sense
 setopt nobeep                   								# No beep

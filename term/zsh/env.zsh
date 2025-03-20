d2b46b91929da6af254bc58f23f351074b474d0d
Gideon Shaked <gideonshaked@gmail.com>
2025-03-20 12:58:23 -0400

    Update environment variable assignments in Zsh configuration

--- term/zsh/env.zsh
+++ term/zsh/env.zsh
@@ -2,9 +2,9 @@
 # Useful environment variables
 #
 
-$CLASSES="$HOME/Documents/umich/classes"
-$DEV="$HOME/Documents/personal/dev"
-$DOTFILES="$HOME/Documents/personal/dev/dotfiles"
+CLASSES="$HOME/Documents/umich/classes"
+DEV="$HOME/Documents/personal/dev"
+DOTFILES="$HOME/Documents/personal/dev/dotfiles"
 
-$CV="$HOME/Documents/personal/dev/resume"
-$WEBSITE="$HOME/Documents/personal/dev/gideonshaked.github.io"
+CV="$HOME/Documents/personal/dev/resume"
+WEBSITE="$HOME/Documents/personal/dev/gideonshaked.github.io"
